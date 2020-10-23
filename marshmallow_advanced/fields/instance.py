b3b8587eeb9555b6b2b41fde2d65f11697ebfdb3
egvo <work.egvo@ya.ru>
2020-10-23 17:43:12 +0400

    Added Active field.

--- marshmallow_advanced/fields/instance.py
+++ marshmallow_advanced/fields/instance.py
@@ -1,14 +1,8 @@
 import typing
 
-from marshmallow.fields import *
+from marshmallow.fields import Field
 from mongoengine import ValidationError as MongoValidationError, Document, QuerySet, InvalidQueryError, ObjectIdField
 from mongoengine.base import TopLevelDocumentMetaclass
-
-
-__all__ = ["Field", "Raw", "Nested", "Mapping", "Dict", "List", "Tuple", "String", "UUID", "Number", "Integer",
-           "Decimal", "Boolean", "Float", "DateTime", "NaiveDateTime", "AwareDateTime", "Time", "Date", "TimeDelta",
-           "Url", "URL", "Email", "IP", "IPv4", "IPv6", "Method", "Function", "Str", "Bool", "Int", "Constant",
-           "Pluck", "Instance"]
 
 
 class Instance(Field):
